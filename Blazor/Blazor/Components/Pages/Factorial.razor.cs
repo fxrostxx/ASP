4f6021d9ea2c3eabd499925b1e4157568c312d44
fxrostxx <fxrostxx@FXROSTXX-PC>
2026-08-31 20:45:19 +0300

    Factorial separated

---
+++ Blazor/Blazor/Components/Pages/Factorial.razor.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+	public partial class Factorial
+	{
+		int num = 0;
+		BigInteger factorial = 1;
+		void Calculate()
+		{
+			factorial = 1;
+			for (int i = 1; i <= num; ++i) factorial *= i;
+		}
+	}
+}
