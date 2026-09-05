80c76dd451e771c0eb4f8414cf642afdcdf82209
fxrostxx <fxrostxx@FXROSTXX-PC>
2026-09-05 17:26:10 +0300

    Movies start

---
+++ Movies/Movies/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Models
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public DateOnly ReleaseDate { get; set; }
+		public string Genre { get; set; }
+	}
+}
