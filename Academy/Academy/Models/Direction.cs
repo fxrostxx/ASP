2fdc86301e1bc2406fb8898d35e9a3b6e85d3f4e
fxrostxx <fxrostxx@FXROSTXX-PC>
2026-09-16 19:49:31 +0300

    Groups and Directions navigation properties

--- Academy/Academy/Models/Direction.cs
+++ Academy/Academy/Models/Direction.cs
@@ -14,5 +14,7 @@ namespace Academy.Models
 		[StringLength(50, MinimumLength = 2)]
 		[Column(TypeName = "NVARCHAR(50)")]
 		public string direction_name { get; set; }
+
+		public ICollection<Group> Groups { get; set; }
 	}
 }
